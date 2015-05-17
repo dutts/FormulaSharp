37358f4f6d95965c9c387766957a9e356ca6fbe1
Richard <richard@dutton.me.uk>
2015-05-17 18:47:56 +0100

    Initial commit of some Types

---
+++ FormulaSharp/Types.fs
@@ -0,0 +1,43 @@
+﻿namespace FormulaSharp
+
+type Country = string
+
+type Driver = {
+    TLA: string;
+    FirstName: string;
+    LastName: string;
+    Nationality: Country;
+    PaceModifier: decimal;
+}
+
+type Team = {
+    Name: string;
+    Drivers: Driver seq;
+}
+
+type TrackRegionPaceModifier = {
+    Name: string;
+    IsEnabled: bool;
+    Modifier: decimal;
+}
+
+type TrackRegion = {
+    Name: string;
+    Pace: decimal;
+    PaceModifiers: TrackRegionPaceModifier seq;    
+}
+
+type Track = {
+    OutLap: TrackRegion seq;
+    FlyingLap: TrackRegion seq;
+    InLap: TrackRegion seq;
+    OutInLap: TrackRegion seq;
+}
+
+type Circuit = {
+    Name: string;
+    Country: Country;
+    Track: Track;
+    Laps: int;
+}
+
