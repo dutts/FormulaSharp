30daccd7f24beb551ec1b5296851595976d44703
dutts <richard@dutton.me.uk>
2017-01-23 22:26:13 +0000

    Some general scripting

--- FormulaSharp/Script.fsx
+++ FormulaSharp/Script.fsx
@@ -1,8 +1,36 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
+#load "Types.fs"
 open FormulaSharp
 
 // Define your library scripting code here
 
+let team : Team = { Name = "Team 1" }
+
+let driver1 = { Number = 1; TLA = "DUT"; Team = team; FirstName = "Richard"; LastName = "Dutton"; Nationality = "British"; PaceModifier = 0.00M }
+let driver2 = { Number = 2; TLA = "RIC"; Team = team; FirstName = "Rich"; LastName = "Dutts"; Nationality = "British"; PaceModifier = 0.00M }
+let drivers = [ driver1; driver2 ]
+
+let track = { OutLap = 80M; FlyingLap = 77.142M; InLap = 79M; OutInLap = 75M }
+let monaco = { Name = "Monaco"; Country = "Monaco"; Track = track; Laps = 60 }
+
+let advanceDriverByLap circuitLapPace driver =
+    circuitLapPace + driver.Pace
+    
+let sortedTiming timing =
+    timing
+    |>Seq.sortBy (fun f -> f.Elapsed)
+
+let runSim drivers circuit =
+    let lapSimData = Seq.empty
+    let lapLoop = 
+        for lap = 1 to circuit.Laps do 
+//            let driverLoop = 
+//                for driver in drivers do
+//                    let previousLapTime = lapSimData.find (fun t -> t.Driver = driver && t.Lap = lap) 
+//                    let driverLapTime = advanceDriverByLap circuit.Track.FlyingLap driver
+//                    lapSimData |> Seq.append { Driver = driver; Lap = lap; Elapsed = previousLapTime.Elapsed + driverLapTime}
+//                    ignore
+        
+    lapSimData
--- FormulaSharp/Types.fs
+++ FormulaSharp/Types.fs
@@ -2,42 +2,50 @@
 
 type Country = string
 
-type Driver = {
-    TLA: string;
-    FirstName: string;
-    LastName: string;
-    Nationality: Country;
-    PaceModifier: decimal;
-}
-
 type Team = {
-    Name: string;
-    Drivers: Driver seq;
+    Name: string
 }
 
+type Driver = {
+    Number : int
+    TLA: string
+    Team: Team
+    FirstName: string
+    LastName: string
+    Nationality: Country
+    PaceModifier: decimal
+}
 type TrackRegionPaceModifier = {
-    Name: string;
-    IsEnabled: bool;
-    Modifier: decimal;
+    Name: string
+    IsEnabled: bool
+    Modifier: decimal
 }
 
 type TrackRegion = {
-    Name: string;
-    Pace: decimal;
-    PaceModifiers: TrackRegionPaceModifier seq;    
+    Name: string
+    Pace: decimal
+    PaceModifiers: TrackRegionPaceModifier seq  
 }
 
 type Track = {
-    OutLap: TrackRegion seq;
-    FlyingLap: TrackRegion seq;
-    InLap: TrackRegion seq;
-    OutInLap: TrackRegion seq;
+    OutLap: decimal // TrackRegion seq
+    FlyingLap: decimal // TrackRegion seq
+    InLap: decimal // TrackRegion seq
+    OutInLap: decimal // TrackRegion seq
 }
 
 type Circuit = {
-    Name: string;
-    Country: Country;
-    Track: Track;
-    Laps: int;
+    Name: string
+    Country: Country
+    Track: Track
+    Laps: int
 }
+
+type TimingDataLap = {
+    Driver: Driver
+    Lap: int
+    Elapsed: decimal
+}
+
+type TimingData = TimingDataLap seq
 
